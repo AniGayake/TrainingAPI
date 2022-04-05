46b05426560538ba52ef18e6e8a1f2d751e532b5
Aniruddha <anigayake101@gmail.com>
2022-04-05 14:16:12 +0530

    Added Post, put and delete

--- src/main/java/com/aniruddha/btraining/controller/ProductRestController.java
+++ src/main/java/com/aniruddha/btraining/controller/ProductRestController.java
@@ -1,10 +1,19 @@
 package com.aniruddha.btraining.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.aniruddha.btraining.entity.Product;
 import com.aniruddha.btraining.service.ProductService;
 
@@ -18,5 +27,31 @@ public class ProductRestController {
 	public List<Product> list() {
 	    return service.listAll();
 	}
+	
+	@PostMapping("/products")
+	public Product saveProduct(@RequestBody Product product) {
+		service.save(product);
+		
+		return product;
+		
+	}
+	
+	@PutMapping("/products/{id}")
+	public ResponseEntity<?> update(@RequestBody Product product, @PathVariable Integer id){
+		try {
+			Product existProduct = service.get(id);
+			service.save(product);
+			return new ResponseEntity<> (HttpStatus.OK);
+			
+		}catch(NoSuchElementException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("/products/{id}")
+	public void delete(@PathVariable Integer id) {
+		service.delete(id);
+	}
+	
 
 }
